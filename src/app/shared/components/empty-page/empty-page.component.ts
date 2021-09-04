557ee8e3ebdd7d880e3fb671aaf46a3c5f07c532
Ewerton <ewerton.pluri@gmail.com>
2021-09-04 14:47:32 -0300

    refact: changing all ui texts to english (unifying the pt/en fiesta xD)

--- src/app/shared/components/empty-page/empty-page.component.ts
+++ src/app/shared/components/empty-page/empty-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empty-page',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empty-page.component.scss'],
 })
 export class EmptyPageComponent implements OnInit {
+  title: string;
+  description: string;
+
   emptyPageVectorPath: string;
 
   constructor() {}
 
   ngOnInit(): void {
     this.emptyPageVectorPath = '../../../assets/images/empty_page.svg';
+    this.title = 'Oops!';
+    this.description = `This page is not ready yet. We're working on it to be up soon! :)`;
   }
 }
